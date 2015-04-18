22513ab2ee8f8902fd5319795060f4df7d354a0f
Josh Bebbington <josh@focalwebsites.com>
2015-04-18 16:31:33 +0100

    An added job shows up on the homepage

--- spec/job_feature_spec.rb
+++ spec/job_feature_spec.rb
@@ -2,7 +2,19 @@ feature 'Job' do
   context 'Listing all jobs' do
     scenario 'No jobs by default' do
       visit '/'
+
       expect(page).to have_content 'There are currently no jobs'
     end
   end
+
+  context 'With a job added' do
+    scenario 'Displays the job on the homepage' do
+      Job.create(role: 'Product Manager', company: 'Acme Inc.', url: 'http://acme.com/internships/product')
+      visit '/'
+
+      expect(page).to have_link 'Product Manager', href: 'http://acme.com/internships/product'
+      expect(page).to have_content 'Acme Inc.'
+    end
+  end
+
 end
