38784fb886ff6e23a4584c9dabe75846393dc957
Josh Bebbington <josh@focalwebsites.com>
2015-04-20 13:57:04 +0100

    Builds on simple Angel List scraper, validates internships, locations, and posting dates

--- app/services/angel_list.rb
+++ app/services/angel_list.rb
@@ -1,14 +1,32 @@
 require 'httparty'
 require 'json'
 
-file = File.read('app/services/angel_list.json')
-hash = JSON.parse(file)
-internships = []
+def is_internship? job
+  job['job_type'] == 'internship'
+end
 
-hash['jobs'].each do |job|
-  if job['job_type'] == 'internship'
-    internships << job
+def is_in_london? job
+  job['tags'].any? {|hash| hash['display_name'] == 'London'}
+end
+
+def posted_today? job
+  job['created_at'][0..10] == Time.now.strftime("%Y-%m-%d")
+end
+
+def build_internship job
+  Job.create(role: job['title'], url: job['angellist_url'], company: job['startup']['name'])
+end
+
+def create_jobs hash
+  hash['jobs'].each do |job|
+    build_internship(job) if (is_internship?(job) && is_in_london?(job))
   end
 end
 
-
+page = 1
+while Job.all.length < 20
+  response = HTTParty.get("https://api.angel.co/1/jobs?access_token=dc01000daed664c4657b0b04649323766893367905b6dacc;page=#{page}")
+  hash = JSON.parse(response.body)
+  create_jobs(hash)
+  page += 1
+end
