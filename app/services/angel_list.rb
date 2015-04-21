c2b3021c4c086a070d9a5f4e5ae78a4abb0c33d1
Josh Bebbington <josh@focalwebsites.com>
2015-04-21 15:33:30 +0100

    Adds some further styling

--- app/services/angel_list.rb
+++ app/services/angel_list.rb
@@ -13,18 +13,22 @@ def posted_today? job
   job['created_at'][0..10] == Time.now.strftime("%Y-%m-%d")
 end
 
+def is_valid? job
+  is_internship?(job) && is_in_london?(job) && posted_today?(job)
+end
+
 def build_internship job
   Job.create(role: job['title'], url: job['angellist_url'], company: job['startup']['name'])
 end
 
 def create_jobs hash
   hash['jobs'].each do |job|
-    build_internship(job) if (is_internship?(job) && is_in_london?(job))
+    build_internship(job) if is_valid?(job)
   end
 end
 
 page = 1
-while Job.all.length < 20
+5.times do
   response = HTTParty.get("https://api.angel.co/1/jobs?access_token=dc01000daed664c4657b0b04649323766893367905b6dacc;page=#{page}")
   hash = JSON.parse(response.body)
   create_jobs(hash)
