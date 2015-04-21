b3b0d752d43c759a40d8e5d560d75fa4a836ea3f
Luke Clewlow <luke.clewlow@gmail.com>
2015-04-21 19:35:52 +0100

    filtered array

--- app/services/work_in_startups.rb
+++ app/services/work_in_startups.rb
@@ -1,6 +1,6 @@
 require 'mechanize'
 
-mechanize = Mechanize.new
+	mechanize = Mechanize.new
 
 page = mechanize.get('http://workinstartups.com/job-board/jobs/interns/')
 
@@ -8,6 +8,27 @@ page = mechanize.get('http://workinstartups.com/job-board/jobs/interns/')
 #   puts link.href
 # end
 
-page.search("ul.job-list li").each do |x|
-  puts x.to_s[/at\s(.*?)\sin/m, 1]
+# page.search("ul.job-list li").each do |x|
+#   puts x.to_s[/at\s(.*?)\sin/m, 1]
+# end
+
+def pull_data
+	mechanize = Mechanize.new
+	job_array = []
+	page = mechanize.get('http://workinstartups.com/job-board/jobs/interns/')
+	page.search("ul.job-list li").each do |x|
+  	job_array << x.to_s[/at\s(.*?)\sin/m, 1]
+	end
+	return job_array
 end
+
+def filter_crap
+	filtered = pull_data.select! { |title| title.kind_of? String }
+	filtered = filtered.delete_if {|title| title.include?("<")}
+	filtered.each do |x|
+		puts x
+		puts x.class
+	end
+end
+
+pull_data
